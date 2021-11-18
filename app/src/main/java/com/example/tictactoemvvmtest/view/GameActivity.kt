60ced4bf0a8298b69a21249480673e043482e7cd
EhsanTgv <EhsanTgv@gmail.com>
2021-11-18 14:24:57 +0330

    Implement xml layout

--- app/src/main/java/com/example/tictactoemvvmtest/view/GameActivity.kt
+++ app/src/main/java/com/example/tictactoemvvmtest/view/GameActivity.kt
@@ -1,11 +1,12 @@
-package com.example.tictactoemvvmtest
+package com.example.tictactoemvvmtest.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tictactoemvvmtest.R
 
-class MainActivity : AppCompatActivity() {
+class GameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_game)
     }
 }
